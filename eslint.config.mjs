72dcdc1bdf50063e60eb612a00ef2ae8a611e591
Felipe Carneiro <felipesgc@yahoo.com.br>
2025-02-18 23:24:57 -0300

    chore:install-plugins-setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import simpleImportSort from "eslint-plugin-simple-import-sort"; 11.2k (gzipped: 3.9k)
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
